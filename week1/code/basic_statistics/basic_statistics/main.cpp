9eb9224d678f4390db8b7d44db19a91972129b17
nickdiorio <nadiorio@gmail.com>
2017-06-19 14:45:25 -0600

    Week 1 example cpp problem

---
+++ week1/code/basic_statistics/basic_statistics/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "statistics.h"
+
+int main()
+{
+	// get the number of integers
+	int N = 0;
+	std::cout << "Number of integers: ";
+	std::cin >> N;
+
+	// get the integers
+	std::vector<int> numbers;
+	numbers.reserve(N);
+
+	// count statistics as we go
+	double sum = 0.;
+	double mean = 0.;
+	int min = 1e16, max=-1e16;
+	for (int i = 0; i != N; i++)
+	{
+		int tmp = 0;
+		std::cout << "Enter number: ";
+		std::cin >> tmp;
+		numbers.push_back(tmp);
+
+		sum += tmp;
+		if (tmp < min)
+			min = tmp;
+		if (tmp > max)
+			max = tmp;
+	}
+	
+	std::cout << "Using accumulated statistics\n";
+	std::cout << "Mean " << sum / double(N) << "\n";
+	std::cout << "Max: " << max << "\n";
+	std::cout << "Min: " << min << "\n";
+	std::cout << "Range: " << max - min << "\n";
+
+	// OR use user-defined library
+	std::cout << "Using library functions\n";
+	std::cout << "Mean " << vector_mean(numbers) << "\n";
+	std::cout << "Max: " << vector_max(numbers) << "\n";
+	std::cout << "Min: " << vector_min(numbers) << "\n";
+	std::cout << "Range: " << vector_max(numbers) - vector_min(numbers) << "\n";
+	
+	system("pause");
+}
+
---
+++ week1/code/basic_statistics/basic_statistics/statistics.h
@@ -0,0 +1,37 @@
+#ifndef _STATISTICS_H_
+#define _STATISTICS_H_
+
+// O(N) - linear
+double vector_mean(std::vector<int> numbers)
+{
+	double sum = 0.;
+	int N = numbers.size();
+	for (int i = 0; i !=N ; i++)
+		sum += numbers[i];
+	return sum / double(N);
+}
+
+// O(N) - linear
+int vector_max(std::vector<int> numbers)
+{
+	int max = numbers[0];
+	for (int i = 1; i != numbers.size(); i++)
+	{
+		if (numbers[i] > max)
+			max = numbers[i];
+	}
+	return max;
+}
+
+// O(N) - linear
+int vector_min(std::vector<int> numbers)
+{
+	int min = numbers[0];
+	for (int i = 1; i != numbers.size(); i++)
+	{
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
+	return min;
+}
+#endif
