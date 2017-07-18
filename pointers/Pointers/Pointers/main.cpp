f35455f511c4c2b2c8d7ac8b8fc05f69ac9db44a
nickdiorio <nadiorio@gmail.com>
2017-07-18 12:09:36 -0600

    Finish pointer examples

--- pointers/Pointers/Pointers/main.cpp
+++ pointers/Pointers/Pointers/main.cpp
@@ -32,28 +32,39 @@ int main()
 	std::cout << "Result: " << *ptr1 << " for: " << *ptr2 << std::endl << std::endl;
 
 	// Problem 7.5
-	std::cout << "Problem 7.5 \n";
 	char * oddOrEven = "Never odd or even";
 
-	// 7.5.1 points to space
+	// 7.5.1 pointer to 6th char of oddOrEven
+	std::cout << "Problem 7.5.1 \n";
 	char * nthCharPtr = &(oddOrEven[5]);
 
-	// 7.5.2 point to e
+	// 7.5.2 updated nthCharPtr to point to 4th char of oddOrEven
+	std::cout << "Problem 7.5.2 \n";
 	nthCharPtr -= 2;
 
-	// 7.5.3 print e
-	std::cout << *nthCharPtr << std::endl;
+	// 7.5.3 print value pointed to by nthCharPtr
+	std::cout << "Problem 7.5.3 \n";
+	std::cout << *nthCharPtr << std::endl << std::endl;
 
-	// 7.5.4 point to pointer pointing to e
+	// 7.5.4 new pointer to a pointer pointing to nthCharPtr
+	std::cout << "Problem 7.5.4 \n";
 	char ** pointerPtr = &nthCharPtr;
 
-	// 7.5.5 print e
-	std::cout << **pointerPtr << std::endl;
+	// 7.5.5 print value stored in pointerPtr
+	std::cout << "Problem 7.5.5 \n";
+	std::cout << pointerPtr << std::endl << std::endl;
 
-	
+	// 7.5.6 print value pointed to by nthCharPtr using pointerPtr
+	std::cout << "Problem 7.5.6 \n";
+	std::cout << **pointerPtr << std::endl << std::endl;
 
+	// 7.5.7 Update nthCharptr to point to next char in OddOrEven
+	std::cout << "Problem 7.5.7 \n";
+	nthCharPtr += 1;
 
-
+	// 7.5.8 Print how far away from *nthCharPtr is from start of string
+	std::cout << "Problem 7.5.8 \n";
+	std::cout << "Distance from start: " << nthCharPtr - oddOrEven << std::endl << std::endl;
 
 	system("pause");
 	return errorStatus;
